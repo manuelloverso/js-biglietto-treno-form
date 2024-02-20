ea11570ecdba3365036e55177397abaf16f2659e
manuelloverso <manuelloverso@gmail.com>
2024-02-20 15:50:26 +0100

    add structure

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Variables Declaration
+let price;
+const pricePerKm = 0.21;
+
+const userName = document.getElementById("name").value;
+
+const km = document.getElementById("km").value;
+
+let age = document.getElementById("age");
+
+document.getElementById("submit").addEventListener("click", function () {
+  console.log(age.value);
+});
