28577a99c6968861c4919163bd0ff6924103562e
manuelloverso <manuelloverso@gmail.com>
2024-02-20 16:28:15 +0100

    add discount conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,23 @@
 let price;
 const pricePerKm = 0.21;
 
-const userName = document.getElementById("name").value;
-
-const km = document.getElementById("km").value;
-
-let age = document.getElementById("age");
-
 document.getElementById("submit").addEventListener("click", function () {
-  console.log(age.value);
+  const userName = document.getElementById("name").value;
+  const km = document.getElementById("km").value;
+  let age = document.getElementById("age").value;
+
+  console.log(userName);
+  console.log(km);
+  console.log(age);
+
+  price = km * pricePerKm;
+  console.log(price);
+
+  if (age == "senior") {
+    price = price - price * 0.4;
+    console.log(price);
+  } else if (age == "minor") {
+    price = price - price * 0.2;
+    console.log(price);
+  }
 });
